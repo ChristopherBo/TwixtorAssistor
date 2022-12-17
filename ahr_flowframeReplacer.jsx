1420b67d79a7ba6ec78db6422918fd53b659e7ba
ChristopherBo <csb3946@g.rit.edu>
2022-12-16 23:45:18 -0500

    Fixed formatting; started implementing regex

--- ahr_flowframeReplacer.jsx
+++ ahr_flowframeReplacer.jsx
@@ -16,41 +16,67 @@
 	ahr_flowframeReplacer.scriptName = "ahr_flowframeReplacer";
 	ahr_flowframeReplacer.scriptTitle = ahr_flowframeReplacer.scriptName + "v1.0";
     
-        //base checks before starting
-        // Check that a project exists
-        if (app.project === null) {
-            alert("Project does not exist!");
-            return false;
-        }
+    //base checks before starting
+    // Check that a project exists
+    if (app.project === null) {
+        alert("Project does not exist!");
+        return false;
+    }
 
-        // Check that an active comp exists
-        if (app.project.activeItem === null) {
-            alert("There is no active comp!");
-            return false;
-        }
+    // Check that an active comp exists
+    if (app.project.activeItem === null) {
+        alert("There is no active comp!");
+        return false;
+    }
 
-        // Check that there's a selected property
-        if (app.project.activeItem.selectedLayers.length == 0) {
-            alert("There is no selected layers to replace with!");
-            return false;
-        }
+    // Check that there's at least 1 selected item
+    if (app.project.activeItem.selectedLayers.length == 0) {
+        alert("There is no selected layers to replace with!");
+        return false;
+    }
 
-        //Check that the setup was run via checking for twix folder
-        var twixFolder = null;
-        for(var i=1; i < app.project.items.length+1; i++) {
-            if(app.project.items[i] instanceof FolderItem && app.project.items[i].name == "Twixtor Precomps") {
-                twixFolder = app.project.items[i];
+    //Check that the setup was run via checking for twix folder
+    var twixFolder = null;
+    for(var i=1; i < app.project.items.length+1; i++) {
+        if(app.project.items[i] instanceof FolderItem && app.project.items[i].name == "Twixtor Precomps") {
+            twixFolder = app.project.items[i];
+        }
+    }
+    if(twixFolder == null) { //it doesnt exist
+        alert("Twixtor Precomps folder with original twixtor precomps does not exist!");
+        return false;
+    }
+
+    //Check that precomps exist in the twix folder
+    if(twixFolder.numItems < 1) {
+        alert("There are no precomps in the Twixtor Precomps folder!");
+        false
+    }
+    //drop all the selected items in an array
+    var layers = app.project.activeItem.selectedLayers;
+
+    //collect all the comp names for regexing
+    var compNames = [];
+    // for(var i=1; i <= twixFolder.numItems; i++) {
+    //     var comp = twixFolder.item(i);
+    //     compNames.push(comp.name);
+    // }
+
+    for(var i=0; i < layers.length; i++) {
+        //check to see if the comp's name matches any of the layer names
+        for(var j=1; j <= twixFolder.numItems; j++) {
+            if(new RegExp(twixFolder.item(j).name).test(layers[i].name)) {
+                processComp(twixFolder.item(j), layers[i]);
             }
         }
-        if(twixFolder == null) { //it doesnt exist
-            alert("Twixtor Precomps folder with original twixtor precomps does not exist!");
-            return false;
-        }
+    }
 
-        //Check that precomps exist in the twix folder
-        if(twixFolder.items.length < 1) {
-            alert("There are no precomps in the Twixtor Precomps folder!");
-            false
-        }
+    //Given a comp and a layer, places the layer
+    //in the comp and fits the comp to the layer's
+    //framerate and length.
+    //Also moves layer to top of comp.
+    function processComp(comp, layer) {
+        
+    }
 
 })(this);
