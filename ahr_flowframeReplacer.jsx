d59768dd7e14963d88d86a5b85371b0fff650d62
ChristopherBo <csb3946@g.rit.edu>
2022-12-16 23:23:22 -0500

    barebones assistant jsx file

---
+++ ahr_flowframeReplacer.jsx
@@ -0,0 +1,16 @@
+//ahr_flowframeReplacer.jsx Version 1.0
+// Copyright (c) 2022 AHRevolvers. All rights reserved.
+//
+// This is a bonus addon script to my Twixtor Assistor extension.
+// Given a folder of flowframes clips it places all of them in the
+// precomps they were twixtored in and formats their respective precomps
+// to the length and FPS of the flowframed clips.
+//
+// Prereq: The precomps you originally twixtored in must be in a folder
+//         named Twixtor Precomps. If you used my Twixtor Assistor
+//         extension, it should already exist with the precomps in it.
+// Usage: Select the flowframe clips and run it.
+
+(function ahr_flowframeReplacer(thisObj) {
+    
+})(this);
