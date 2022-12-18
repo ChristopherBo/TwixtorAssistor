0fafd6adc86b370cd29884a407b9d828a331ce57
ChristopherBo <csb3946@g.rit.edu>
2022-12-18 12:12:58 -0500

    fixed bug where non-video files would be processed

--- jsx/aftereffects.jsx
+++ jsx/aftereffects.jsx
@@ -1,5 +1,6 @@
 ﻿//todo:
-//mass import/replace original clip with flowframed clips
+//change buttons to ashin's suggestion of ae styling
+//add numpad bindings
 
 "object" != typeof JSON && (JSON = {}), function () { "use strict"; var rx_one = /^[\],:{}\s]*$/, rx_two = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, rx_three = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, rx_four = /(?:^|:|,)(?:\s*\[)+/g, rx_escapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, rx_dangerous = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, gap, indent, meta, rep; function f(t) { return t < 10 ? "0" + t : t } function this_value() { return this.valueOf() } function quote(t) { return rx_escapable.lastIndex = 0, rx_escapable.test(t) ? '"' + t.replace(rx_escapable, function (t) { var e = meta[t]; return "string" == typeof e ? e : "\\u" + ("0000" + t.charCodeAt(0).toString(16)).slice(-4) }) + '"' : '"' + t + '"' } function str(t, e) { var r, n, o, u, f, a = gap, i = e[t]; switch (i && "object" == typeof i && "function" == typeof i.toJSON && (i = i.toJSON(t)), "function" == typeof rep && (i = rep.call(e, t, i)), typeof i) { case "string": return quote(i); case "number": return isFinite(i) ? String(i) : "null"; case "boolean": case "null": return String(i); case "object": if (!i) return "null"; if (gap += indent, f = [], "[object Array]" === Object.prototype.toString.apply(i)) { for (u = i.length, r = 0; r < u; r += 1)f[r] = str(r, i) || "null"; return o = 0 === f.length ? "[]" : gap ? "[\n" + gap + f.join(",\n" + gap) + "\n" + a + "]" : "[" + f.join(",") + "]", gap = a, o } if (rep && "object" == typeof rep) for (u = rep.length, r = 0; r < u; r += 1)"string" == typeof rep[r] && (o = str(n = rep[r], i)) && f.push(quote(n) + (gap ? ": " : ":") + o); else for (n in i) Object.prototype.hasOwnProperty.call(i, n) && (o = str(n, i)) && f.push(quote(n) + (gap ? ": " : ":") + o); return o = 0 === f.length ? "{}" : gap ? "{\n" + gap + f.join(",\n" + gap) + "\n" + a + "}" : "{" + f.join(",") + "}", gap = a, o } } "function" != typeof Date.prototype.toJSON && (Date.prototype.toJSON = function () { return isFinite(this.valueOf()) ? this.getUTCFullYear() + "-" + f(this.getUTCMonth() + 1) + "-" + f(this.getUTCDate()) + "T" + f(this.getUTCHours()) + ":" + f(this.getUTCMinutes()) + ":" + f(this.getUTCSeconds()) + "Z" : null }, Boolean.prototype.toJSON = this_value, Number.prototype.toJSON = this_value, String.prototype.toJSON = this_value), "function" != typeof JSON.stringify && (meta = { "\b": "\\b", "\t": "\\t", "\n": "\\n", "\f": "\\f", "\r": "\\r", '"': '\\"', "\\": "\\\\" }, JSON.stringify = function (t, e, r) { var n; if (indent = gap = "", "number" == typeof r) for (n = 0; n < r; n += 1)indent += " "; else "string" == typeof r && (indent = r); if ((rep = e) && "function" != typeof e && ("object" != typeof e || "number" != typeof e.length)) throw new Error("JSON.stringify"); return str("", { "": t }) }), "function" != typeof JSON.parse && (JSON.parse = function (text, reviver) { var j; function walk(t, e) { var r, n, o = t[e]; if (o && "object" == typeof o) for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (void 0 !== (n = walk(o, r)) ? o[r] = n : delete o[r]); return reviver.call(t, e, o) } if (text = String(text), rx_dangerous.lastIndex = 0, rx_dangerous.test(text) && (text = text.replace(rx_dangerous, function (t) { return "\\u" + ("0000" + t.charCodeAt(0).toString(16)).slice(-4) })), rx_one.test(text.replace(rx_two, "@").replace(rx_three, "]").replace(rx_four, ""))) return j = eval("(" + text + ")"), "function" == typeof reviver ? walk({ "": j }, "") : j; throw new SyntaxError("JSON.parse") }) }();
 var twixFolder = null;
@@ -48,6 +49,7 @@ function setupEnv() {
 
     //grab each layer
     var layers = app.project.activeItem.selectedLayers;
+    //alert(layers[0].name);
     var comp = app.project.activeItem;
     twixtored = []; //reset counter
 
@@ -61,6 +63,10 @@ function setupEnv() {
     var sequentialNames = false;
     var sequentialNameIndex = 1;
     for(var i=0; i < layers.length; i++) {
+        //alert("Layers[i] footage?: " + !(layers[i].source instanceof FootageItem));
+        if(!(layers[i].source instanceof FootageItem)) {
+            return "Layers must be video files!";
+        }
         for(var j=0; j < layers.length; j++) {
             if(layers[i].name == layers[j].name && layers[i] != layers[j]) {
                 //fail case- go to sequential mode
@@ -73,7 +79,7 @@ function setupEnv() {
         }
     }
 
-    //alert("time to precomp");
+    // alert("find twix folder");
     //find twix precomps folder, if dne make it
     for(var i=1; i < app.project.items.length+1; i++) {
         if(app.project.items[i] instanceof FolderItem && app.project.items[i].name == "Twixtor Precomps") {
@@ -109,7 +115,7 @@ function setupEnv() {
         }
     }
 
-    //alert("sequential names");
+    // alert("time to precomp");
     //precomp range of layers
     for(var i=0; i < layers.length; i++) {
         // if(debug.value) { writeToDebugFile("Setting up " + layers[i].name + "\n"); }
@@ -161,6 +167,7 @@ function setupEnv() {
         // if(debug.value) { writeToDebugFile("Finished setting up " + layers[i].name + "\n"); }
     }
     
+    // alert("precomping done");
     //open a comp & its time remap. set playhead to beginning of comp
     twixFolder.item(1).openInViewer();
     var precompedLayer = app.project.activeItem.layers[1];
