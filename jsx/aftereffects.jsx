898a5857740c7818b901a8c4af997e6cdd6ff8d8
ChristopherBo <csb3946@g.rit.edu>
2022-12-18 22:00:54 -0500

    Added an alternative twixtor adding method

--- jsx/aftereffects.jsx
+++ jsx/aftereffects.jsx
@@ -1,6 +1,9 @@
 ﻿//todo:
 //change buttons to ashin's suggestion of ae styling
-//add numpad bindings
+
+//Change this variable to true to enable applying twixtor on the precomps
+//with frame number enabled instead of in the precomps!
+var frameNumberTwixtor = false;
 
 "object" != typeof JSON && (JSON = {}), function () { "use strict"; var rx_one = /^[\],:{}\s]*$/, rx_two = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, rx_three = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, rx_four = /(?:^|:|,)(?:\s*\[)+/g, rx_escapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, rx_dangerous = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, gap, indent, meta, rep; function f(t) { return t < 10 ? "0" + t : t } function this_value() { return this.valueOf() } function quote(t) { return rx_escapable.lastIndex = 0, rx_escapable.test(t) ? '"' + t.replace(rx_escapable, function (t) { var e = meta[t]; return "string" == typeof e ? e : "\\u" + ("0000" + t.charCodeAt(0).toString(16)).slice(-4) }) + '"' : '"' + t + '"' } function str(t, e) { var r, n, o, u, f, a = gap, i = e[t]; switch (i && "object" == typeof i && "function" == typeof i.toJSON && (i = i.toJSON(t)), "function" == typeof rep && (i = rep.call(e, t, i)), typeof i) { case "string": return quote(i); case "number": return isFinite(i) ? String(i) : "null"; case "boolean": case "null": return String(i); case "object": if (!i) return "null"; if (gap += indent, f = [], "[object Array]" === Object.prototype.toString.apply(i)) { for (u = i.length, r = 0; r < u; r += 1)f[r] = str(r, i) || "null"; return o = 0 === f.length ? "[]" : gap ? "[\n" + gap + f.join(",\n" + gap) + "\n" + a + "]" : "[" + f.join(",") + "]", gap = a, o } if (rep && "object" == typeof rep) for (u = rep.length, r = 0; r < u; r += 1)"string" == typeof rep[r] && (o = str(n = rep[r], i)) && f.push(quote(n) + (gap ? ": " : ":") + o); else for (n in i) Object.prototype.hasOwnProperty.call(i, n) && (o = str(n, i)) && f.push(quote(n) + (gap ? ": " : ":") + o); return o = 0 === f.length ? "{}" : gap ? "{\n" + gap + f.join(",\n" + gap) + "\n" + a + "}" : "{" + f.join(",") + "}", gap = a, o } } "function" != typeof Date.prototype.toJSON && (Date.prototype.toJSON = function () { return isFinite(this.valueOf()) ? this.getUTCFullYear() + "-" + f(this.getUTCMonth() + 1) + "-" + f(this.getUTCDate()) + "T" + f(this.getUTCHours()) + ":" + f(this.getUTCMinutes()) + ":" + f(this.getUTCSeconds()) + "Z" : null }, Boolean.prototype.toJSON = this_value, Number.prototype.toJSON = this_value, String.prototype.toJSON = this_value), "function" != typeof JSON.stringify && (meta = { "\b": "\\b", "\t": "\\t", "\n": "\\n", "\f": "\\f", "\r": "\\r", '"': '\\"', "\\": "\\\\" }, JSON.stringify = function (t, e, r) { var n; if (indent = gap = "", "number" == typeof r) for (n = 0; n < r; n += 1)indent += " "; else "string" == typeof r && (indent = r); if ((rep = e) && "function" != typeof e && ("object" != typeof e || "number" != typeof e.length)) throw new Error("JSON.stringify"); return str("", { "": t }) }), "function" != typeof JSON.parse && (JSON.parse = function (text, reviver) { var j; function walk(t, e) { var r, n, o = t[e]; if (o && "object" == typeof o) for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (void 0 !== (n = walk(o, r)) ? o[r] = n : delete o[r]); return reviver.call(t, e, o) } if (text = String(text), rx_dangerous.lastIndex = 0, rx_dangerous.test(text) && (text = text.replace(rx_dangerous, function (t) { return "\\u" + ("0000" + t.charCodeAt(0).toString(16)).slice(-4) })), rx_one.test(text.replace(rx_two, "@").replace(rx_three, "]").replace(rx_four, ""))) return j = eval("(" + text + ")"), "function" == typeof reviver ? walk({ "": j }, "") : j; throw new SyntaxError("JSON.parse") }) }();
 var twixFolder = null;
@@ -140,6 +143,14 @@ function setupEnv() {
         }
         var precompLayer = comp.layers[layerIndex];
 
+        //add twix on precompLayer if needed
+        if(frameNumberTwixtor) {
+            addTwixtor(precompLayer);
+            try {  //actually add the frame numbering thing
+                precompLayer.Effects("Twixtor Pro")("Time Remap Mode").setValue(2);
+            } catch(e) {}
+        }
+
         //precomp fits the same area and same duration as original
         // precomp.displayStartTime = precompLayer.inPoint - precompLayer.startTime;
         //the next 5 lines are mythical and are not to be changed under any circumstances
@@ -237,25 +248,18 @@ function nextButton(renderQueue, twixtor) {
     //mark it as finished by adding it to the twixtored list & add to render queue
     twixtored.push(app.project.activeItem);
     //alert("renderqueue: " + typeof(renderQueue) + " " + renderQueue);
-    if(renderQueue == true) {
+    if(renderQueue) {
         alert("rendering queue");
         app.executeCommand(app.findMenuCommandId("Add to Render Queue"));
         alert("done");
     }
     //alert("twix: " + typeof(twixtor) + " " + twixtor);
-    if(twixtor == true) {
+    if(twixtor && frameNumberTwixtor == false) {
         if(checkForTwixtor() == false) {
             alert("Twixtor is not installed! Continuing...");;
         }
         //alert("twixtoring...");
-        app.project.activeItem.layers[1]("Effects").addProperty("Twixtor Pro");
-        try {
-            app.project.activeItem.layers[1].Effects("Twixtor Pro")("In FPS is Out FPS").setValue(0);
-        } catch (e) {}
-        try {
-            app.project.activeItem.layers[1].Effects("Twixtor Pro")("Output Control")("Time Remap Mode").setValue(1);
-        } catch (e) {}
-        app.project.activeItem.layers[1].Effects("Twixtor Pro")("Input: Frame Rate").setValue(23.976);
+        addTwixtor(app.project.activeItem.layers[1]);
         //alert("done");
     }
 
@@ -380,6 +384,18 @@ function checkForTwixtor(){
         }
     }
     return false;
+}
+
+//adds twixtor to a given layer
+function addTwixtor(layer) {
+    layer("Effects").addProperty("Twixtor Pro");
+    try {
+        layer.Effects("Twixtor Pro")("In FPS is Out FPS").setValue(0);
+    } catch (e) {}
+    try {
+        layer.Effects("Twixtor Pro")("Output Control")("Time Remap Mode").setValue(1);
+    } catch (e) {}
+    layer.Effects("Twixtor Pro")("Input: Frame Rate").setValue(23.976);
 }
 
 //Does basic checks to make sure a given button can be clicked.
